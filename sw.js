db140f14bc173c9448a5b1ca4912666e56750494
mikkelsander774 <144893671+mikkelsander774@users.noreply.github.com>
2026-08-24 18:34:35 +0200

    Tilføj service worker

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'aa-samling-v1';
+const ASSETS = ['/Jumbobog/', '/Jumbobog/index.html', '/Jumbobog/manifest.json'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Network first: forsøg altid at hente ny version, fald tilbage til cache
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
